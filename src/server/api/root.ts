08a714b218abd98743e8f03bdad6a7653685b59b
Davon Davis <davond009@gmail.com>
2024-01-30 10:18:19 -0500

    add trpc people procedure

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
+import {peopleRouter} from "~/server/api/routers/people";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  people: peopleRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/people.ts
@@ -0,0 +1,16 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import axios from "axios";
+
+export type People = {
+  firstName: string;
+  lastName: string;
+};
+
+export const peopleRouter = createTRPCRouter({
+  getPeople: protectedProcedure.query(async (): Promise<People[]> => {
+    const result = await axios.get("http://localhost:8080/people");
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return result.data;
+  }),
+});
